40396d9a3ca6920625689155e95e089a3c0d8598
Nikolas Koesling <nikolas@koesling.info>
2022-08-15 17:52:54 +0200

    restructure "desktop" mode

---
+++ guided-commands.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function guided_modbus_tcp_client_shm {
+    echo "${FUNCNAME[0]}"
+}
+
+function guided_modbus_rtu_client_shm {
+    echo "${FUNCNAME[0]}"
+}
+
+function guided_dump_shm {
+    echo "${FUNCNAME[0]}"
+}
+
+function guided_write_shm {
+    echo "${FUNCNAME[0]}"
+}
+
+function guided_shared_mem_random {
+    echo "${FUNCNAME[0]}"
+}
+
+function guided_stdin_to_modbus_shm {
+    echo "${FUNCNAME[0]}"
+}
+
---
+++ manual-commands.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function manual_modbus_tcp_client_shm {
+    echo "${FUNCNAME[0]}"
+}
+
+function manual_modbus_rtu_client_shm {
+    echo "${FUNCNAME[0]}"
+}
+
+function manual_dump_shm {
+    echo "${FUNCNAME[0]}"
+}
+
+function manual_write_shm {
+    echo "${FUNCNAME[0]}"
+}
+
+function manual_shared_mem_random {
+    echo "${FUNCNAME[0]}"
+}
+
+function manual_stdin_to_modbus_shm {
+    echo "${FUNCNAME[0]}"
+}
+
